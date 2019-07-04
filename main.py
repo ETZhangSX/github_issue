5331372671862b1339edc1bef4dea0ebe8ffe102
ETZhangSX <etzhangsx@qq.com>
2019-07-04 22:00:34 +0800

    添加对issue相关信息的统计

--- main.py
+++ main.py
@@ -1,14 +1,49 @@
+import sys
+from urllib.parse import urlparse
 from spiders import github_issue_spider
 from models.model_issue import Issue
 
+github_issue_url = [
+            'https://github.com/TarsCloud/Tars/issues',
+            'https://github.com/Tencent/TSeer/issues',
+            'https://github.com/Tencent/DCache/issues',
+]
+
+def help():
+    helpMsg = '''This
+    '''
+    print(helpMsg)
+    return
+
 if __name__ == '__main__':
-    github_tars_issue_url = 'https://github.com/TarsCloud/Tars/issues'
-    print('开始爬虫...')
-    print('开始爬取issue list')
-    issue_list = github_issue_spider.get_issues(github_tars_issue_url)
-    print('获取issue list结束，开始获取issue评论...')
-    issue_detail = github_issue_spider.get_all_issues_detail(issue_list)
-    print('获取issue评论信息完毕')
+    # if len(sys.argv) == 1:
+    #     help()
+    #     return
+    param = sys.argv[1]
+    if "spider" == param:
+
+        print('开始爬虫...')
+        for url in github_issue_url:
+            print('开始爬取issue list: %s' % url)
+            issue_list = github_issue_spider.get_issues(url)
+            print('获取issue list结束，开始获取issue评论...')
+            issue_detail = github_issue_spider.get_all_issues_detail(issue_list)
+            print('获取issue评论信息完毕')
+
+    elif "info" == param:
+        issue = Issue()
+        for url in github_issue_url:
+            issue.issue_info("'%s'" % urlparse(url).path.split('/')[2])
+
+
+    # else:
+    #     paramError()
+    #     return
+    # return
+
+
     # issue = Issue()
+    # res = issue.count('issue', "status='closed'")
+
     # issue.save_all(issue_detail)
 
--- models/model_issue.py
+++ models/model_issue.py
@@ -1,8 +1,9 @@
 from config.database import HOST, PORT, USER, PASSWORD, DATABASE
 import pymysql
+import datetime
 import logging
 
-
+utc_format = '%Y-%m-%dT%H:%M:%SZ'
 
 class Issue(object):
     def __del__(self):
@@ -32,17 +33,17 @@ class Issue(object):
     def save_all(self, issue_list):
 
         for issue in issue_list:
-            sql = """INSERT INTO issue(title, source, type, No, time, answered, status, author, link, content)
-                             values ('%s', '%s', '%s','%s', '%s', '%s', '%s','%s', '%s')""" % (
-            issue['title'], issue['source'], issue['type'], issue['id'], issue['time'], issue['answered'], issue['status'], issue['link'], issue['content'])
+            sql = """INSERT INTO issue(title, source, type, No, opened_time, latest_time, answered, status, author, link, content)
+                             values ('%s', '%s', '%s','%s', '%s', '%s', '%s', '%s','%s', '%s')""" % (
+            issue['title'], issue['source'], issue['type'], issue['id'], issue['opened_time'], issue['latest_time'], issue['answered'], issue['status'], issue['link'], issue['content'])
 
             try:
                 self.cursor.execute(sql)
                 self.db.commit()
             except:
                 try:  # 插入失败表示数据库存在次issue，转为update更新
-                    sql_update = """UPDATE issue SET title='%s', type='%s', time='%s', answered='%s', status='%s', content='%s' WHERE No='%s'""" % (
-                    issue['title'], issue['type'], issue['time'], issue['answered'], issue['status'], issue['content'], issue['id'])
+                    sql_update = """UPDATE issue SET title='%s', type='%s', opened_time='%s', latest_time='%s', answered='%s', status='%s', content='%s' WHERE No='%s'""" % (
+                    issue['title'], issue['type'], issue['opened_time'], issue['latest_time'], issue['answered'], issue['status'], issue['content'], issue['id'])
                     self.cursor.execute(sql_update)
                     self.db.commit()
                     print("更新完成")
@@ -51,9 +52,9 @@ class Issue(object):
                     self.db.rollback()
 
     def save_one(self, issue):
-        sql = """INSERT INTO issue(title, source, type, No, time, answered, status, author, link, content)
-                                         values('%s', '%s', '%s','%s', '%s', '%s', '%s','%s' ,'%s', '%s')""" % (
-            issue['title'], issue['source'], issue['type'], issue['id'], issue['time'], issue['answered'], issue['status'], issue['author'], issue['link'], issue['content'])
+        sql = """INSERT INTO issue(title, source, type, No, opened_time, latest_time, answered, status, author, link, content)
+                                         values('%s', '%s', '%s','%s', '%s', '%s', '%s', '%s','%s' ,'%s', '%s')""" % (
+            issue['title'], issue['source'], issue['type'], issue['id'], issue['opened_time'], issue['latest_time'], issue['answered'], issue['status'], issue['author'], issue['link'], issue['content'])
 
         try:
             self.cursor.execute(sql)
@@ -62,11 +63,71 @@ class Issue(object):
         except Exception as e:
             try:  # 插入失败表示数据库存在次issue，转为update更新
                 print("Exception: %s" % e)
-                sql_update = """UPDATE issue SET title='%s', type='%s', time='%s', answered='%s', status='%s', content='%s' WHERE No='%s'""" % (
-                    issue['title'], issue['type'], issue['time'], issue['answered'], issue['status'], issue['content'], issue['id'])
+                sql_update = """UPDATE issue SET title='%s', type='%s', opened_time='%s', latest_time='%s', answered='%s', status='%s', content='%s' WHERE No='%s'""" % (
+                    issue['title'], issue['type'], issue['opened_time'], issue['latest_time'], issue['answered'], issue['status'], issue['content'], issue['id'])
                 self.cursor.execute(sql_update)
                 self.db.commit()
                 print("%s : Update successfully" % issue['id'])
             except Exception as e:
                 logging.error("update error:%s" % e)
                 self.db.rollback()
+
+    # 获取对应源的issue信息
+    def issue_info(self, source):
+        total_count = self.get_issue_count(source)
+        closed_count = self.get_issue_count(source, "status='closed'")
+        select_results = self.select('issue', 'opened_time, latest_time', source, "status='closed'")
+
+
+
+        avg_time = datetime.timedelta()
+
+        for item in select_results:
+            opened_time = datetime.datetime.strptime(item[0], utc_format)
+            closed_time = datetime.datetime.strptime(item[1], utc_format)
+
+            avg_time += (closed_time - opened_time)
+
+        avg_time /= len(select_results)
+
+        print('''
+Issues Info of %s :
+        total count: %s
+        closed count: %s
+        average opening time: %s
+        ''' % (source, total_count, closed_count, avg_time))
+
+
+    def get_issue_count(self, source, condition=""):
+        return self.count(table='issue', source=source, condition=condition)
+
+
+    def select(self, table, column, source, condition):
+        if condition == "":
+            sql = "SELECT %s FROM %s WHERE source=%s" % (column, table, source)
+        else:
+            sql = "SELECT %s FROM %s WHERE source=%s and %s" % (column, table, source, condition)
+
+        try:
+            self.cursor.execute(sql)
+            result = self.cursor.fetchall()
+            return result
+        except Exception as e:
+            print("Error occur: %s" % e)
+
+
+    def count(self, table, source, condition=""):
+        if condition == "":
+            sql = "SELECT count(1) FROM %s WHERE source=%s" % (table, source)
+        else:
+            sql = "SELECT count(1) FROM %s WHERE source=%s and %s" % (table, source, condition)
+        res = 0
+
+        try:
+            self.cursor.execute(sql)
+            result = self.cursor.fetchall()
+            res = result[0][0]
+        except Exception as e:
+            print("Error: fetch data error with message: %s" % e)
+
+        return res
